03e0ff989d35ab5ecfc2f0c10be6d50db9f11237
Elliott <ellduc4@gmail.com>
2024-02-22 13:15:22 -0600

    Create ElementUtility

---
+++ DialogueSystem/Editor/Utilities/ElementUtility.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+
+namespace DialogueSystem.Editor.Utilities
+{
+	///Allows easy creation of UI elements.
+	public static class ElementUtility
+	{
+		public static Button CreateButton(string text, Action onClick) => new(onClick) { text = text };
+
+		public static Foldout CreateFoldout(string title, bool collapsed = false) => new() { text = title, value = !collapsed };
+
+		public static Port CreatePort(this Node node, Direction direction, Port.Capacity capacity) => node.InstantiatePort(Orientation.Horizontal, direction, capacity, typeof(bool));
+
+		public static TextField CreateTextField(string value, string label, EventCallback<ChangeEvent<string>> onChange)
+		{
+			TextField textField = new() { value = value, label = label };
+			textField.RegisterValueChangedCallback(onChange);
+			return textField;
+		}
+
+		public static TextField CreateTextArea(string value, string label, EventCallback<ChangeEvent<string>> onChange)
+		{
+			var textArea = CreateTextField(value, label, onChange);
+			textArea.multiline = true;
+			return textArea;
+		}
+	}
+}
