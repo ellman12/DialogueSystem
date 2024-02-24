196ae73882a475d26f9d59e9de90617528270106
Elliott <ellduc4@gmail.com>
2024-02-24 15:04:56 -0600

    Ports can now be connected

--- DialogueSystem/Editor/Window/DialogueGraphView.cs
+++ DialogueSystem/Editor/Window/DialogueGraphView.cs
@@ -2,6 +2,8 @@ using DialogueSystem.Data;
 using DialogueSystem.Editor.Utilities;
 using DialogueSystem.Editor.Elements;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine.UIElements;
 using UnityEngine;
@@ -47,6 +49,11 @@ namespace DialogueSystem.Editor.Window
 				_ => throw new ArgumentOutOfRangeException()
 			};
 		}
+		
+        public override List<Port> GetCompatiblePorts(Port startPort, NodeAdapter _)
+		{
+			return ports.Where(port => startPort != port && startPort.node != port.node && startPort.direction != port.direction).ToList();
+        }
 		
 		private IManipulator CreateNodeContextualMenu(string actionTitle, DialogueType type) => new ContextualMenuManipulator(menuEvent => menuEvent.menu.AppendAction(actionTitle, actionEvent => AddElement(CreateNode(type, GetLocalMousePosition(actionEvent.eventInfo.localMousePosition)))));
 
