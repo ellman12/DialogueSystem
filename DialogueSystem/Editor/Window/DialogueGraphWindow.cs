8e26ab578b8294edbe72a6905616edb37c11d9ca
Elliott <ellduc4@gmail.com>
2024-02-25 16:30:06 -0600

    Remove graphView field

--- DialogueSystem/Editor/Window/DialogueGraphWindow.cs
+++ DialogueSystem/Editor/Window/DialogueGraphWindow.cs
@@ -4,14 +4,12 @@ namespace DialogueSystem.Editor.Window
 {
 	public sealed class DialogueGraphWindow : EditorWindow
 	{
-		private DialogueGraphView graphView;
-
 		[MenuItem("Window/Dialogue Graph")]
 		public static void Open() => GetWindow<DialogueGraphWindow>("Dialogue Graph");
 
 		private void OnEnable()
 		{
-			graphView = new DialogueGraphView(this);
+			rootVisualElement.Add(new DialogueGraphView(this));
 		}
 	}
 }
