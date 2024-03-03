fe7687f4533e1850dd5558b3889dcf0157480774
Elliott <ellduc4@gmail.com>
2024-03-02 19:45:01 -0600

    Add Alt C testing hotkey to clear the Graphs folder

--- DialogueSystem/Editor/Window/DialogueGraphWindow.cs
+++ DialogueSystem/Editor/Window/DialogueGraphWindow.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using DialogueSystem.Editor.Utilities;
 using UnityEditor;
 
@@ -8,6 +9,15 @@ namespace DialogueSystem.Editor.Window
 		[MenuItem("Window/Dialogue Graph")]
 		public static void Open() => GetWindow<DialogueGraphWindow>("Dialogue Graph");
 
+		//TODO: delete this later
+		[MenuItem("DS/Clear &c")]
+		public static void Clear(MenuCommand menuCommand)
+		{
+			Directory.Delete("Assets/DialogueSystem/Graphs", true);
+			Directory.CreateDirectory("Assets/DialogueSystem/Graphs");
+			AssetDatabase.Refresh();
+		}
+		
 		private void OnEnable()
 		{
 			rootVisualElement.AddStyleSheet("Constants");
