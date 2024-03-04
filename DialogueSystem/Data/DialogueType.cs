a45487ea522b1ecddf0053c2f90064512a0443b9
Elliott <ellduc4@gmail.com>
2024-03-03 19:26:00 -0600

    Change DialogueType to reflect renaming and refactoring

--- DialogueSystem/Data/DialogueType.cs
+++ DialogueSystem/Data/DialogueType.cs
@@ -2,7 +2,10 @@
 {
     public enum DialogueType
     {
-        SingleChoice,
-        MultipleChoice
+        ///The node is just for storing text, without presenting the player with any kind of choice.
+        Text,
+        
+        ///The node prompts the user with at least one choice.
+        Prompt
     }
 }
