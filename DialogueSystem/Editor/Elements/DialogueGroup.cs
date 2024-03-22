87d6c207bcdca89f9a7c172998c01a18a1a70175
Elliott <ellduc4@gmail.com>
2024-03-22 01:22:53 -0500

    Basic start to GroupSaveData

--- DialogueSystem/Editor/Elements/DialogueGroup.cs
+++ DialogueSystem/Editor/Elements/DialogueGroup.cs
@@ -1,20 +1,20 @@
 using System;
+using DialogueSystem.Data;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 namespace DialogueSystem.Editor.Elements
 {
-    [Serializable]
-    public sealed class DialogueGroup : Group
-    {
-        public string Id = Guid.NewGuid().ToString();
-        public Vector2 Position;
-        
-        public DialogueGroup(Vector2 position)
-        {
-            title = "New Group";
-            Position = position;
-            SetPosition(new Rect(position, Vector2.zero));
-        }
-    }
+	[Serializable]
+	public sealed class DialogueGroup : Group
+	{
+		public GroupSaveData SaveData;
+
+		public DialogueGroup(Vector2 position)
+		{
+			SaveData = GroupSaveData.Create(position);
+			title = "New Group";
+			SetPosition(new Rect(position, Vector2.zero));
+		}
+	}
 }
