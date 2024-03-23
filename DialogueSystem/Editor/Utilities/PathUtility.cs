fe4f194aa86535e27d03a0d6b78d0abfeaf0c7fc
Elliott <ellduc4@gmail.com>
2024-03-23 16:17:44 -0500

    Move GetRelativePath to new PathUtility class

---
+++ DialogueSystem/Editor/Utilities/PathUtility.cs
@@ -0,0 +1,18 @@
+﻿namespace DialogueSystem.Editor.Utilities
+{
+	///Various DialogueSystem utility methods for paths.
+	public static class PathUtility
+	{
+		///Takes an absolute path and returns it relative to Assets/.
+		public static string GetRelativePath(string fullPath)
+		{
+			string path = fullPath.Replace('\\', '/').Replace(Constants.ProjectRoot, "");
+			
+			//Remove pesky / at the start, which breaks AssetDatabase.CreateAsset().
+			if (path.StartsWith('/'))
+				path = path[1..];
+
+			return path;
+		}
+	}
+}
