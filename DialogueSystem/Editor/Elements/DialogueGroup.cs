9c1d0bd4f4fc821940f66b0f2fb9d95245f4e013
Elliott <ellduc4@gmail.com>
2024-03-25 12:35:29 -0500

    This needs lots of cleanup but loading groups works

--- DialogueSystem/Editor/Elements/DialogueGroup.cs
+++ DialogueSystem/Editor/Elements/DialogueGroup.cs
@@ -21,6 +21,16 @@ namespace DialogueSystem.Editor.Elements
 			SetPosition(new Rect(position, Vector2.zero));
 		}
 
+		public DialogueGroup(GroupSaveData saveData)
+		{
+			SaveData = saveData;
+			
+			RegisterCallback<FocusOutEvent>(_ => FocusOut());
+
+			title = SaveData.Name;
+			SetPosition(new Rect(SaveData.Position, Vector2.zero));
+		}
+
 		private void FocusOut()
 		{
 			SaveData.Name = title = title.Trim();
