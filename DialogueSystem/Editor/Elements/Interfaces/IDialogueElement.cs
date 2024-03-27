6ede712a0d7cde0aaec66e9cd2bc208feeba18f0
Elliott <ellduc4@gmail.com>
2024-03-26 19:26:48 -0500

    Create IDialogueElement, IMovableElement, and ISaveableElement

---
+++ DialogueSystem/Editor/Elements/Interfaces/IDialogueElement.cs
@@ -0,0 +1,9 @@
+﻿namespace DialogueSystem.Editor.Elements.Interfaces
+{
+	///Shared behavior between any element on the DialogueGraphView: nodes, edges, and groups.
+	public interface IDialogueElement
+	{
+		///Removes the element from the DialogueGraphView, preserving its SaveData asset.
+		public void Remove();
+	}
+}
---
+++ DialogueSystem/Editor/Elements/Interfaces/ISaveableElement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace DialogueSystem.Editor.Elements.Interfaces
+{
+	///Represents a Dialogue Element that has some type of SaveData attached to it.
+	public interface ISaveableElement<TAsset> : IDialogueElement where TAsset : ScriptableObject
+	{
+		public TAsset SaveData { get; set; }
+		
+		///Removes the element from the DialogueGraphView, deleting its SaveData asset.
+		public void Delete();
+	}
+}
