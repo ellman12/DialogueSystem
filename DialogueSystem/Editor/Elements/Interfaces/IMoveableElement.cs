ef4059c404af83dc47ca92464d76461ca61bcadd
Elliott <ellduc4@gmail.com>
2024-03-26 19:26:48 -0500

    Create IDialogueElement, IMovableElement, and ISaveableElement

---
+++ DialogueSystem/Editor/Elements/Interfaces/IMoveableElement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace DialogueSystem.Editor.Elements.Interfaces
+{
+	///Represents a Dialogue Element (like nodes and groups) that can be clicked and dragged around.
+	public interface IMoveableElement
+	{
+		public void UpdatePosition(Vector2 newPosition);
+	}
+}
