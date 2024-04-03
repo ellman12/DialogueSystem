367aaedba0bce0ee84b8414f8c16b3f2456685a5
Elliott <ellduc4@gmail.com>
2024-04-03 16:43:53 -0500

    Add comments to Conversation.cs, and add SelectChoice()

--- Scripts/Conversation.cs
+++ Scripts/Conversation.cs
@@ -9,17 +9,19 @@ namespace DialogueSystem.Scripts
 	{
 		public NodeSaveData Current { get; private set; }
 
-		public event EventHandler OnBegin, OnAdvance, OnEnd;
+		public event EventHandler OnBegin, OnAdvance, OnChoiceSelected, OnEnd;
 
 		[SerializeField]
 		private NodeSaveData start;
 
+		///Start the conversation by setting Current to the start node.
 		public void Begin()
 		{
 			OnBegin?.Invoke(null, EventArgs.Empty);
 			Current = start;
 		}
 
+		///Advance to the next text node.
 		public void Advance()
 		{
 			OnAdvance?.Invoke(null, EventArgs.Empty);
@@ -28,6 +30,16 @@ namespace DialogueSystem.Scripts
 				Current = Current.Next;
 		}
 
+		///Advances the conversation to the selected choice node. Indexes are 1-based.
+		public void SelectChoice(int index)
+		{
+			if (index < 1 || index >= Current.Choices.Count + 1) return;
+			
+			OnChoiceSelected?.Invoke(null, EventArgs.Empty);
+			Current = Current.Choices[index - 1].Node;
+		}
+
+		///Finish the conversation by setting Current to null.
 		public void End()
 		{
 			OnEnd?.Invoke(null, EventArgs.Empty);
